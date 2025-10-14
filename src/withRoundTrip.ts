4682a6f50a77a8efe4b8994f8a1a46aa08a6d5cb
Michael Fouquet <mfouquet@users.noreply.github.com>
2025-10-14 10:18:26 -0500

    🔥 Better clean up of unused files

--- src/withRoundTrip.ts
+++ src/withRoundTrip.ts
@@ -1,17 +1,9 @@
-import { Result } from "src/types";
 import { useEffect, useChannel } from "storybook/preview-api";
 import type { DecoratorFunction } from "storybook/internal/types";
-
 import { EVENTS } from "./constants";
 import { RuleResult, scanPage } from "@stark-contrast/rule-engine";
 
-/**
- * This is an example of a function that performs some sort of analysis on the
- * canvas. In this example, it returns the bounding rectangles for elements that
- * - have a style attribute
- * - are divs with fewer than 2 childNodes
- */
-const check = (canvas: ParentNode = globalThis.document) => {
+const check = () => {
   const results = scanPage(globalThis.document, { snippetLength: 1000 });
   const violations = results.filter((r: RuleResult) => r?.result === "FAIL");
   const potentials = results.filter(
@@ -26,14 +18,13 @@ const check = (canvas: ParentNode = globalThis.document) => {
 };
 
 export const withRoundTrip: DecoratorFunction = (storyFn, context) => {
-  const canvasElement = context.canvasElement as ParentNode;
   const emit = useChannel({
     [EVENTS.REQUEST]: () => {
-      emit(EVENTS.RESULT, check(canvasElement));
+      emit(EVENTS.RESULT, check());
     },
   });
   useEffect(() => {
-    emit(EVENTS.RESULT, check(canvasElement));
+    emit(EVENTS.RESULT, check());
   });
 
   return storyFn();
